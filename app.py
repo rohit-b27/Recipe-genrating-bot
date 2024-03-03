cb8ceaf3aba0bd82f8a9098d6cd36f18cfc83fa8
rohit-b27 <bandlamudirohit@gmail.com>
2024-03-03 02:28:02 -0600

    initial trails

---
+++ app.py
@@ -0,0 +1,31 @@
+# chatbot using streamlit 
+import langchain 
+import os 
+import openai
+from langchain_community.llms import HuggingFaceHub
+from langchain_community.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import SimpleSequentialChain
+from langchain_openai import ChatOpenAI
+from langchain.schema import HumanMessage,SystemMessage,AIMessage
+import streamlit as st 
+
+#response generation: 
+def response(question):
+    llm = OpenAI(model ="gpt-3.5-turbo-0125", api_key = os.environ['openai_api_key'],temperature = 0.7 )
+    response = llm.invoke(question)
+    return response
+
+# streamlit 
+st.set_page_config(page_title="Cookbook")
+st.header('Chatbot')
+
+input = st.text_input("input: ", key ="input")
+output = response(input)
+
+submit =st.button("Generate response")
+
+if submit:
+    st.subheader("The response is")
+    st.write(output)
